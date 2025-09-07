26d70d5a301b0e881057522ce9254bf7acff90ac
LiorYanuka <yanukalior@gmail.com>
2025-09-07 22:43:30 +0300

    Styling changes

--- Frontend/tspo/src/components/Input.tsx
+++ Frontend/tspo/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, KeyboardEvent } from "react";
+import type { ChangeEvent, KeyboardEvent } from "react";
 
 interface InputProps {
   type?: "text" | "email" | "password" | "number";
